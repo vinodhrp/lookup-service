12fc94585a6d694f89974dd0bbd38b510ea8c91e
Vinodh Ramamoorthy <vinodh.rp@gmail.com>
2019-06-05 12:00:46 +0530

    removed registry

--- src/main/java/com/spring/cloud/lookup/LookupServiceApplication.java
+++ src/main/java/com/spring/cloud/lookup/LookupServiceApplication.java
@@ -3,17 +3,14 @@ package com.spring.cloud.lookup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
-@EnableEurekaClient
+//@EnableEurekaClient
 public class LookupServiceApplication {
 	
 	@Bean
-	@LoadBalanced
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
 	}
